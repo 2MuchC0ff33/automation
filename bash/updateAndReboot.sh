ab13b9fe5fd4fe91db3d39a8d2e3ff1be37d8afe
2MuchCoff33 <65397884+2MuchC0ff33@users.noreply.github.com>
2023-08-27 12:57:32 +0800

    Update updateAndReboot.sh

--- bash/updateAndReboot.sh
+++ bash/updateAndReboot.sh
@@ -8,17 +8,17 @@ sudo apt update -y
 # Upgrade the system.
 sudo apt upgrade -y
 
+# Clean up the system.
+sudo apt autoremove -y
+sudo apt clean -y
+sudo apt autoclean -y
+
 # Check if a reboot is necessary.
-if needs-restarting; then
+if systemctl --failed &> /dev/null; then
     echo "Reboot required."
     sudo reboot
 else
     echo "No reboot required."
 fi
-
-# Clean up the system.
-sudo apt autoremove -y
-sudo apt clean -y
-sudo apt autoclean -y
 
 # End of script.
